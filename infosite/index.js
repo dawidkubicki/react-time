9db0b8e5648fb840a3ffe15e39d37c7d85a42faa
Dawid Kubicki <dawid@pluscode.io>
2022-04-23 13:22:48 +0200

    element render

---
+++ infosite/index.js
@@ -0,0 +1,20 @@
+const element = (
+ 
+        <nav>
+            <h1>Pluscode</h1>
+            <ul>
+                <li>
+                    Pricing
+                </li>
+                <li>
+                    About
+                </li>
+                <li>
+                    Contact
+                </li>
+            </ul>
+        </nav>
+
+)
+
+ReactDOM.render(element, document.getElementById("root"))
